c9c6c72236d0ef1297baf86a0405bef5ecf4ef42
vietthai.3011 <ngovietthai3011@gmail.com>
2025-02-08 21:59:18 +0700

    bai 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    // jsx
+    render() {
+        return (
+            <div>
+                My first Component
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
