5576bfb0237d55caf2b8880693ff37dd7373c8a6
vietthai.3011 <ngovietthai3011@gmail.com>
2025-02-09 20:29:41 +0700

    bai 52

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,16 @@
 import React from "react";
+import videoHomepage from "../../assets/video-homepage.mp4";
 
 const HomePage = (props) => {
-    return <div className="home-page">HomePage</div>;
+    console.log(videoHomepage);
+
+    return (
+        <div className="home-page">
+            <video width="500" height="500" autoPlay muted loop>
+                <source src={videoHomepage} type="video/mp4" />
+            </video>
+        </div>
+    );
 };
 
 export default HomePage;
