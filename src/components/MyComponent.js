0a5d4f73c385889f91e85af80d0773547e74d1c4
vietthai.3011 <ngovietthai3011@gmail.com>
2025-02-09 09:28:57 +0700

    bai 19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,11 +7,21 @@ class MyComponent extends React.Component {
         address: "HaiDuong",
         age: "22",
     };
+
+    handleOnClick(event) {
+        console.log("My name is " + this.state.name);
+    }
+
+    handleOnMouseOver(event) {
+        console.log(event.pageX);
+    }
     render() {
         return (
-            <div>
+            <>
                 My name is {this.state.name} and i am from {this.state.address}
-            </div>
+                <button onClick={(event) => this.handleOnClick(event)}>Click me!</button>
+                <button onMouseOver={(event) => this.handleOnMouseOver(event)}>Hover Me</button>
+            </>
         );
     }
 }
